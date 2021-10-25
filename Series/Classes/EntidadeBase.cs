f10ea9804fa93be3d5bb70af1988ebd7b1506f9e
Luis Henrique Farina <62112031+kikizoide@users.noreply.github.com>
2021-10-25 10:42:43 -0300

    Initial Setup
    Small TV series CRM project in .NET

---
+++ Series/Classes/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
